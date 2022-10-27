6f4dd3eab83757db31ad930f21083f6188c1ebd9
Joshua Engelkes <jbengelkes@gmail.com>
2022-10-27 14:51:34 -0500

    Setup the client-side echo main function and imports

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -1,4 +1,21 @@
 package echoserver;
 
+import java.net.*;
+import java.io*;
+
 public class EchoClient {
+
+  public static final int PORT_NUMBER = 6013;
+  
+  public static void main(String[] args) {
+  
+    // If a server is taken as an argument, then us that server. Otherwise, default server is 127.0.0.1
+    String server;
+    if (args.length == 0) {
+      server = "127.0.0.1";
+    } else {
+      server = args[0];
+    }
+
+  }  
 }

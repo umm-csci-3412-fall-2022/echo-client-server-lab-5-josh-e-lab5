6e3ea2e30ebfacc0049e88633678e1ab03fe5363
Joshua Engelkes <jbengelkes@gmail.com>
2022-10-27 11:36:51 -0500

    Set up Echo Server sockets for communication between the server and client

--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -1,4 +1,20 @@
 package echoserver;
 
 public class EchoServer {
+
+  final public static int PORT_NUMBER = 6013;
+
+  public static main(String[] args) {
+  
+    try {
+      // Create a server socket for communication link between client and server
+      ServerSocket socket = new ServerSocket(PORT_NUMBER);
+
+      while(true) {
+	// Create the socket to communicate between server and client
+        Socket clientSocketConnection = serverSocket.accept();
+
+      }
+    }
+  }
 }

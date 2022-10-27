5d2b652708130dd091f8b60b37d772a57696e35b
Joshua Engelkes <jbengelkes@gmail.com>
2022-10-27 15:26:13 -0500

    Finish writing out the input from the server into the system

--- src/echoserver/EchoClient.java
+++ src/echoserver/EchoClient.java
@@ -1,7 +1,8 @@
+
 package echoserver;
 
 import java.net.*;
-import java.io*;
+import java.io.*;
 
 public class EchoClient {
 
@@ -16,6 +17,39 @@ public class EchoClient {
     } else {
       server = args[0];
     }
+    
+    try { 
+      // create a socket to make a connection to the server from the client
+      Socket SocketToServer = new Socket(server, PORT_NUMBER);
 
+      // Grab the input stream connected to the output of the client communication socket
+      InputStream input = SocketToServer.getInputStream();
+      // Grab the output stream connected to the input of the client communication socket
+      OutputStream output = SocketToServer.getOutputStream();
+
+      // Create variable to track the bytes being redirected
+      int nextByteFromSystem = 0;
+
+      // Reads the stream from the System and puts the stream into the output
+      while ((nextByteFromSystem = System.in.read()) != -1) {
+        output.write(nextByteFromSystem);
+	output.flush();
+	System.out.flush();
+      }
+      SocketToServer.shutdownOutput();
+
+      // Reset variable to track the bytes being redirected
+      int nextByteFromOutput = 0;
+
+      // Reads the stream from the System and puts the stream into the output
+      while ((nextByteFromOutput = input.read()) != -1) {
+        System.out.write(nextByteFromOutput);
+	System.out.flush();
+      }
+
+    } catch (IOException exception){
+      System.out.println("Unexpected exception: "  + exception);
+    }
   }  
 }
+

e8be7af7ae331e6b4c22b74198fe4aaeb45c46cd
Joshua Engelkes <jbengelkes@gmail.com>
2022-10-27 11:52:30 -0500

    Take the input from the client and send it back, before closing communications

--- src/echoserver/EchoServer.java
+++ src/echoserver/EchoServer.java
@@ -4,7 +4,7 @@ public class EchoServer {
 
   final public static int PORT_NUMBER = 6013;
 
-  public static main(String[] args) {
+  public static void main(String[] args) {
   
     try {
       // Create a server socket for communication link between client and server
@@ -14,6 +14,17 @@ public class EchoServer {
 	// Create the socket to communicate between server and client
         Socket clientSocketConnection = serverSocket.accept();
 
+	// Grab the input stream connected to the output of the client communication socket
+	InputStream input = clientSocketConnection.getInputStream();
+	// Grab the output stream connected to the input of the client communication socket
+	OutputStream output = clientSocketConnection.getOutputStream();
+
+	// Reads the input stream and puts the stream into the output
+	input.transferTo(output);
+
+	input.close();
+	output.close();
+	clientSocketConnection.close();
       }
     }
   }
